4929629db50001bb7f871538912dbe8264c94e68
bigcharinthon <bigcharinthon124@gmail.com>
2026-05-12 16:32:49 +0700

    Add SQL query to analyze shipping cost impact on profit

---
+++ sql/shipping_cost_impact.sql
@@ -0,0 +1,8 @@
+--business quetion: ค่าส่งในแต่ละประเทศส่งผลต่อกำไรอย่างไร
+SELECT 
+	Country,
+	sum(Profit) as totalprofit,
+	sum(Shipping_Cost) as totalshipping
+from global_ecommerce_sales
+group by Country
+order by totalprofit desc
